14b5695fa985440576f954751e8edb968df4a7aa
sayan 367 <sayansarkar90900@gmail.com>
2024-08-07 19:05:17 +0530

    Module 5 is added to the repositotry

---
+++ Module_5/Question1.py
@@ -0,0 +1,8 @@
+# Take input from the user
+number = int(input("Enter a number: "))
+
+# Check if the number is even or odd using the modulus operator
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
---
+++ Module_5/question5.py
@@ -0,0 +1,15 @@
+# Take three numbers as input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+# Determine the largest number
+if num1 >= num2 and num1 >= num3:
+    largest = num1
+elif num2 >= num1 and num2 >= num3:
+    largest = num2
+else:
+    largest = num3
+
+# Print the largest number
+print(f"The largest number is: {largest}")
