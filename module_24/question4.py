117640c17e5da05b6fde8912648e3b23976313ca
sayan 367 <sayansarkar90900@gmail.com>
2024-09-09 05:34:08 +0530

    Question4.py has been added

---
+++ module_24/question4.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+# Create a 3x3 matrix with values ranging from 2 to 10
+matrix = np.arange(2, 11).reshape(3, 3)
+print("3x3 matrix with values ranging from 2 to 10: \n", matrix)
