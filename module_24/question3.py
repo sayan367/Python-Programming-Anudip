9f8fb516acebfaecab3c3a3259c0e44411e7f4fb
sayan 367 <sayansarkar90900@gmail.com>
2024-09-09 05:32:51 +0530

    Question 3 has been added

---
+++ module_24/question3.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Create an array of 10 zeros
+zeros_array = np.zeros(10)
+print("Array of 10 zeros: ", zeros_array)
+
+# Create an array of 10 ones
+ones_array = np.ones(10)
+print("Array of 10 ones: ", ones_array)
+
+# Create an array of 10 fives
+fives_array = np.full(10, 5)
+print("Array of 10 fives: ", fives_array)
